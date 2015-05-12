673d5cce234b1e82d52171cde1c27a5e4725109c
jhonny-wang <543050768@qq.com>
2015-05-13 00:06:07 +0800

    修复一些bug

--- cookie.js
+++ cookie.js
@@ -9,15 +9,17 @@ util.setCookie = function(name,value,expiredays,path){
 
 util.getCookie = function(name){
 	if(document.cookie.length>0){
-		var start = document.cookie.indexOf(name);
-		start = start + 1;
+		var start = document.cookie.indexOf(name + "=");
+		start = start + name.length +1;
 		var end = document.cookie.indexOf(";",start);
 		if(end == -1) end = document.cookie.length;
-		return decondeURI(document.cookie.substring(start,end));
+		return decodeURI(document.cookie.substring(start,end));
 	}
 	return "";
 }
 
 util.removeCookie = function(name){
-	document.cookie = name + "=;expires=" + (new Date(0)).toUTCString();
+	var exdate = new Date();
+	exdate.setDate(exdate.getDate() + '-1');
+	document.cookie = name + "=;expires=" + exdate.toUTCString();
 }
