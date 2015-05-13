08287843d73e264d428e73183b739665a9d26f15
jhonny-wang <543050768@qq.com>
2015-05-14 01:46:12 +0800

    增加一些功能

--- cookie.js
+++ cookie.js
@@ -1,25 +1,43 @@
-var util = util || {};
+;(function(global){
+	var util = util || {};
 
-util.setCookie = function(name,value,expiredays,path){
-	var exdate = new Date();
-	exdate.setDate(exdate.getDate() + expiredays);
-	document.cookie = name + "=" + encodeURI(value) +
-	((expiredays == null) ? "" : ";expires=" + exdate.toUTCString()) + ";path=" + path;
-}
-
-util.getCookie = function(name){
-	if(document.cookie.length>0){
-		var start = document.cookie.indexOf(name + "=");
-		start = start + name.length +1;
-		var end = document.cookie.indexOf(";",start);
-		if(end == -1) end = document.cookie.length;
-		return decodeURI(document.cookie.substring(start,end));
+	function min(target,source){
+		for(var prop in source){
+			if(!source.hasOwnProperty(prop)) continue;
+			target[prop] = source[prop];
+		}
+		return target;
 	}
-	return "";
-}
 
-util.removeCookie = function(name){
-	var exdate = new Date();
-	exdate.setDate(exdate.getDate() + '-1');
-	document.cookie = name + "=;expires=" + exdate.toUTCString();
-}
+	util.setCookie = function(name,value,options){
+		var exdate = new Date();
+		exdate.setDate(exdate.getDate() + options.expires);
+		document.cookie = name + "=" + encodeURI(value) +
+		((options.expires) ? ";expires=" + exdate.toUTCString() : "") + ";path=" + options.path; +
+		";domain=" + options.domain + (options.secure ? ";secure" : "");
+	}
+
+	util.getCookie = function(name){
+		if(document.cookie.length>0){
+			var start = document.cookie.indexOf(name + "=");
+			start = start + name.length +1;
+			var end = document.cookie.indexOf(";",start);
+			if(end == -1) end = document.cookie.length;
+			return decodeURI(document.cookie.substring(start,end));
+		}
+		return "";
+	}
+
+	util.removeCookie = function(name,options){
+		options = min(options,{expires:-1});
+		util.setCookie(name,'',options);
+	}
+
+	if(typeof define === 'function' && define.amd){
+		define(function(){
+			return util;
+		})
+	}else{
+		global.util = util;
+	}
+})(window);
